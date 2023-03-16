ae8d3922f457315a6c35b986588bfdb1cd3b0e9e
Alexzhuan <liangxz06@163.com>
2023-03-16 11:20:25 +0800

    feat: update Text2DT check script

--- format_checker/format_checker_Text2DT.py
+++ format_checker/format_checker_Text2DT.py
@@ -1,6 +1,5 @@
 import json
 import sys
-import datetime
 from format_checker import FormatChecker
 
 class Text2DT(FormatChecker):
@@ -21,6 +20,7 @@ class Text2DT(FormatChecker):
             for block in json_content:
                 text = block['text']
                 tree = block['tree']
+                #print('processing: ', text)
                 for rule in tree:
                     assert self.check_required_fields(rule), '{} are required. The defect record is "{}", and the defect rule is {}.'.format(self.required_field_list, text, rule)
                     assert self.check_field_type(rule['triples'], list), '"triples" must be list, The defect record is "{}", and the defect rule is {}.'.format(text, rule)
@@ -28,7 +28,6 @@ class Text2DT(FormatChecker):
                     assert self.check_predefined_list(rule['logical_rel']), '"logical_rel" must be in predefined list. The defect record is "{}", and the defect rule is {}.'.format(text, rule)
                 # check if it is a valid pre-order binary tree:
                 assert self._check_valid_preorder_tree(tree), 'A valid pre-order binary tree is not satisfied. Each leaf node must be a Decision node, and each Condition node must have two child nodes(left-child and right-child), and EMPTY Decision node "{}" is required if necessary. The defect record is "{}"'.format(EMPTY_DECISION_NODE, text)
-                #assert len(tree) % 2 == 1, 'A valid binary tree must contain odd nodes. The defect record is {}, nodes number is: {}'.format(text, len(tree))
 
     def _check_valid_preorder_tree(self, tree):
         nodelist = []
@@ -38,10 +37,8 @@ class Text2DT(FormatChecker):
         # 将符合诊疗决策树的节点前序序列转化为代表诊疗决策树结构的节点矩阵，matrix[i][j]='F'/'L'/'R'表示第j个节点是第i个节点的父/左子/右子节点
         node_matrix = [[0 for i in range(len(nodelist))] for j in range(len(nodelist))]
 
-        # If it is a valid pre-order tree, the parse process will succeed in a short time, and return True
-        # If it is running a long time, return as False
-        start_time = datetime.datetime.now()
         while nodelist[0] != 'D':
+            raw_nodelist = nodelist.copy()
             for i in range(len(nodelist)):
                 if nodelist[i] == 'C':
                     flag, leaf1, leaf2 = 0, 0, 0
@@ -67,12 +64,8 @@ class Text2DT(FormatChecker):
                             break
                     if flag == 2:
                         break
-
-            end_time = datetime.datetime.now()
-
-            # check program running time:
-            if (end_time - start_time).seconds > 1:
-                print(start_time, end_time)
+            # After each iteration, the nodelist should change, or it will go to dead-loop
+            if raw_nodelist == nodelist:
                 return False
 
         return True
@@ -93,8 +86,3 @@ if __name__ == '__main__':
     checker.check_format(submission_filename)
 
     print("Format Check Success!")
-
-
-
-
-
